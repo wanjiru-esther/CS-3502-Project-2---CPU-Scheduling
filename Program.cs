50a2f7a39848c577f3c78233e3a5e66e78138baa
Wanjiru Esther <100499476+wanjiru-esther@users.noreply.github.com>
2025-04-11 20:06:40 -0400

    Add files via upload
    Initial project setup with Process class and Program.cs
    I created Process class to represent process control blocks (PCBs). I Includes fields for arrival time, burst time, priority, remaining time, etc. to support both preemptive and non-preemptive scheduling. I added constructors and string override for easy display. I set up basic Program.cs structure for scheduling simulation entry point.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Process
+{
+    public int ProcessId { get; set; }
+    public int ArrivalTime { get; set; }
+    public int BurstTime { get; set; }
+    public int RemainingTime { get; set; } // For preemptive algorithms
+    public int Priority { get; set; }
+    public int StartTime { get; set; }
+    public int CompletionTime { get; set; }
+    public int WaitingTime { get; set; }
+    public int TurnaroundTime { get; set; }
+    public int ResponseTime { get; set; }
+
+    public Process(int id, int arrival, int burst, int priority = 0)
+    {
+        ProcessId = id;
+        ArrivalTime = arrival;
+        BurstTime = burst;
+        RemainingTime = burst;
+        Priority = priority;
+        StartTime = -1;
+        CompletionTime = -1;
+        WaitingTime = 0;
+        TurnaroundTime = 0;
+        ResponseTime = -1;
+    }
+
+    public override string ToString()
+    {
+        return $"ID: {ProcessId}, Arrival: {ArrivalTime}, Burst: {BurstTime}, Priority: {Priority}";
+    }
+}
